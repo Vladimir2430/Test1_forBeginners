27d79c80ca6d4ecbba42e5834acdf7b24a17b7a4
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-25 01:25:53 +0300

    code improved

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+
+module.exports = {
+  mode: 'production',
+  entry: './less/less.js',
+  output: {
+    path: path.resolve(__dirname, ''),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: { minimize: true }
+            },
+            {
+              loader: 'less-loader'
+            }
+          ]
+        })
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['env', {
+                targets: {
+                  browsers: "> 3%"
+                }
+              }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.jpg$/,
+        loader: 'file-loader',
+        options: {
+          name: 'images/[name].[ext]'
+        }
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/'
+            }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new ImageminPlugin({test: /\.jpg$/})
+  ],
+  devServer: {
+    host: 'localhost',
+    port: 8000,
+    contentBase: __dirname + ''
+  }
+}
